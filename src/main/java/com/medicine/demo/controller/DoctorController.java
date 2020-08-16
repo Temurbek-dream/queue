f6607794f33c2d2a6a95cb9ffbe0e81fe2c4e12c
Temurbek <temurbek.zaripov140996@mail.ru>
2020-08-16 03:22:36 -0700

    Last step

--- src/main/java/com/medicine/demo/controller/DoctorController.java
+++ src/main/java/com/medicine/demo/controller/DoctorController.java
@@ -23,22 +23,29 @@ public class DoctorController
         this.doctorRepository = doctorRepository;
     }
 
+    //All doctors
     @GetMapping("/all")
     public List<Doctor> getAllDoctors()
     {
         return doctorRepository.findAll();
     }
+
+    //Adding a doctor
     @PostMapping("/add")
     public ResponseEntity<String> addDoctors(@RequestBody Doctor doctor)
     {
         doctorRepository.save(doctor);
         return  new ResponseEntity<>("New doctor is added", HttpStatus.CREATED);
     }
+
+    //Getting a doctor by id
     @GetMapping("/all/{id}")
     public Optional<Doctor> findDoctor(@PathVariable Long id)
     {
         return doctorRepository.findById(id);
     }
+
+    //Changing a doctor
     @PutMapping("/change")
     public ResponseEntity<Doctor> changingParam(@PathVariable Long id,
                                                 @RequestBody Doctor currentdoctor)
@@ -55,10 +62,12 @@ public class DoctorController
       return ResponseEntity.ok(newStaff);
 
     }
+
+    //Be in queue
     @GetMapping("/order")
     public int counting(@RequestParam(name = "nameDoctor") String nameDoctor)
     {
-      Optional<Doctor> doctor= Optional.ofNullable(doctorRepository.findByNameDoctor(nameDoctor));
+      Optional<Doctor> doctor = Optional.ofNullable(doctorRepository.findByNameDoctor(nameDoctor));
        if (doctor.isPresent())
         {
             counting++;
--- src/main/java/com/medicine/demo/entity/Doctor.java
+++ src/main/java/com/medicine/demo/entity/Doctor.java
@@ -11,15 +11,13 @@ public class Doctor
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 
-//    @Column(nullable = false)
     private Long id;
 
-//    @Column(nullable = false)
     private String nameDoctor;
 
-//    @Column(nullable = false)
     private String occupation;
 
-////    @Column(nullable = false)
     private int number;
+
+
 }
